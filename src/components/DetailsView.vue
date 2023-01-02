3fcc2dbdeaad49dcf8c47db7376ecfb1dc5a69ac
TheDarkSid3r <sid3rdark@gmail.com>
2023-01-02 02:54:05 -0800

    add details toggle

--- src/components/DetailsView.vue
+++ src/components/DetailsView.vue
@@ -17,6 +17,7 @@ export default defineComponent({
         currentPage: string;
         showAllPackages: boolean;
         showFilePaths: boolean;
+        showDetails: boolean;
     } {
         return {
             exp: null,
@@ -28,7 +29,8 @@ export default defineComponent({
             ],
             currentPage: "packages",
             showAllPackages: false,
-            showFilePaths: true
+            showFilePaths: true,
+            showDetails: false
         };
     },
     computed: {
@@ -107,6 +109,10 @@ export default defineComponent({
                 <button :class="{ 'nav-link': true, 'active': currentPage === page.id }" @click="currentPage = page.id" href="#">{{ page.name }}</button>
             </li>
         </ul>
+        <div class="form-check mt-4">
+            <input class="form-check-input" type="checkbox" id="show-details" v-model="showDetails" />
+            <label class="form-check-label" for="show-details">Show expansive details</label>
+        </div>
         <template v-if="currentPage === 'packages'">
             <h2 class="mb-3 mt-4">Action packages</h2>
             <div class="form-check">
@@ -129,38 +135,41 @@ export default defineComponent({
                 <tbody>
                     <tr v-for="pkg in packages">
                         <th scope="row">{{ pkg.id }}</th>
-                        <td><a :href="`#package-${pkg.id}-details`">{{ pkg.name }}</a></td>
+                        <td v-if="showDetails"><a :href="`#package-${pkg.id}-details`">{{ pkg.name }}</a></td>
+                        <td v-else>{{ pkg.name }}</td>
                         <td v-if="showAllPackages"><code>{{ getPackageTypeName(pkg.type) }}</code></td>
                         <td v-if="showFilePaths && !showAllPackages"><code>project/actions/{{ pkg.id }}.swf</code></td>
                     </tr>
                 </tbody>
             </table>
-            <h1 class="mb-3 mt-5">Details</h1>
-            <template v-for="pkg in packages">
-                <h4 class="mb-3 pt-3" :id="`package-${pkg.id}-details`">{{ pkg.name }}</h4>
-                <table class="table">
-                    <thead>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Parameters</th>
-                    </thead>
-                    <tbody>
-                        <tr v-for="action in pkg.actions">
-                            <th scope="row">{{ action.id }}</th>
-                            <td>{{ action.name }}</td>
-                            <td class="p-0">
-                                <table class="table" :style="{ 'margin-bottom': '-1px' }" v-if="action.params.length">
-                                    <tbody>
-                                        <tr v-for="param in action.params">
-                                            <td>{{ param.name }}</td>
-                                            <td class="help" :title="getActionParamTypeName(param.type)">{{ param.type }}</td>
-                                        </tr>
-                                    </tbody>
-                                </table>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
+            <template v-if="showDetails">
+                <h1 class="mb-3 mt-5">Details</h1>
+                <template v-for="pkg in packages">
+                    <h4 class="mb-3 pt-3" :id="`package-${pkg.id}-details`">{{ pkg.name }}</h4>
+                    <table class="table">
+                        <thead>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Parameters</th>
+                        </thead>
+                        <tbody>
+                            <tr v-for="action in pkg.actions">
+                                <th scope="row">{{ action.id }}</th>
+                                <td>{{ action.name }}</td>
+                                <td class="p-0">
+                                    <table class="table" :style="{ 'margin-bottom': '-1px' }" v-if="action.params.length">
+                                        <tbody>
+                                            <tr v-for="param in action.params">
+                                                <td>{{ param.name }}</td>
+                                                <td class="help" :title="getActionParamTypeName(param.type)">{{ param.type }}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </template>
             </template>
         </template>
         <template v-if="currentPage === 'flowcharts'">
@@ -201,32 +210,35 @@ export default defineComponent({
                 <tbody>
                     <tr v-for="template in exp!.templates">
                         <th scope="row">{{ template.id }}</th>
-                        <td><a :href="`#template-${template.id}-details`">{{ template.name }}</a></td>
+                        <td v-if="showDetails"><a :href="`#template-${template.id}-details`">{{ template.name }}</a></td>
+                        <td v-else>{{ template.name }}</td>
                         <td>{{ template.params.join(", ") }}</td>
                     </tr>
                 </tbody>
             </table>
-            <h1 class="mb-3 mt-5">Details</h1>
-            <template v-for="template in exp!.templates">
-                <h4 class="mb-3 pt-3" :id="`template-${template.id}-details`">{{ template.name }}</h4>
-                <table class="table">
-                    <thead>
-                        <th scope="col">ID</th>
-                        <th scope="col">Name</th>
-                        <th scope="col">Type</th>
-                        <th scope="col">Default value</th>
-                        <th scope="col">TS variable</th>
-                    </thead>
-                    <tbody>
-                        <tr v-for="field in template.fields">
-                            <th scope="row">{{ field.id }}</th>
-                            <td>{{ field.name }}</td>
-                            <td class="help" :title="getTemplateFieldTypeName(field.type)">{{ field.type }}</td>
-                            <td><code v-if="field.def">"{{ field.def }}"</code></td>
-                            <td><code v-if="field.var">{{ field.var }}</code></td>
-                        </tr>
-                    </tbody>
-                </table>
+            <template v-if="showDetails">
+                <h1 class="mb-3 mt-5">Details</h1>
+                <template v-for="template in exp!.templates">
+                    <h4 class="mb-3 pt-3" :id="`template-${template.id}-details`">{{ template.name }}</h4>
+                    <table class="table">
+                        <thead>
+                            <th scope="col">ID</th>
+                            <th scope="col">Name</th>
+                            <th scope="col">Type</th>
+                            <th scope="col">Default value</th>
+                            <th scope="col">TS variable</th>
+                        </thead>
+                        <tbody>
+                            <tr v-for="field in template.fields">
+                                <th scope="row">{{ field.id }}</th>
+                                <td>{{ field.name }}</td>
+                                <td class="help" :title="getTemplateFieldTypeName(field.type)">{{ field.type }}</td>
+                                <td><code v-if="field.def">"{{ field.def }}"</code></td>
+                                <td><code v-if="field.var">{{ field.var }}</code></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </template>
             </template>
         </template>
     </template>
