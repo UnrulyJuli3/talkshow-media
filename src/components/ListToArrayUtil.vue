859777914e3d3d7b100f7269ad614d9a1a84e098
TheDarkSid3r <sid3rdark@gmail.com>
2023-06-04 18:27:34 -0700

    bracket lol

--- src/components/ListToArrayUtil.vue
+++ src/components/ListToArrayUtil.vue
@@ -4,16 +4,20 @@ import { defineComponent } from "vue";
 export default defineComponent({
     data(): {
         input: string;
+        shouldAddBrackets: boolean;
         reader: FileReader;
     } {
         return {
             input: "",
+            shouldAddBrackets: true,
             reader: new FileReader()
         };
     },
     computed: {
         output() {
-            return this.input.split(/\r?\n/g).map(s => s.trim()).filter(s => s).map(s => `"${s.replace(/"/g, '\\"')}"`).join(",");
+            const res = JSON.stringify(this.input.split(/\r?\n/g).map(s => s.trim()).filter(s => s));
+            if (this.shouldAddBrackets) return res;
+            return res.replace(/^\[/, "").replace(/\]$/, "");
         },
         onUpload() {
             const input = (this.$refs.upload as HTMLInputElement);
@@ -30,6 +34,9 @@ export default defineComponent({
 <template>
     <h2>input:</h2>
     <input type="file" accept="text/*" ref="upload" @input="onUpload" />
+    <br>
+    <input type="checkbox" id="should-add-brackets-checkbox-bc-i-forgot-to-add-this-b4" v-model="shouldAddBrackets" />
+    <label for="should-add-brackets-checkbox-bc-i-forgot-to-add-this-b4">bracket lol</label>
     <textarea :rows="20" :cols="60" v-model="input"></textarea>
     <h2>output:</h2>
     <textarea :rows="20" :cols="60" :value="output" readonly></textarea>
